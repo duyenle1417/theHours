92df80050522b7b1bd5ae1ec3aa7f3335ced9d9d
duyenle1417 <18520680@gm.uit.edu.vn>
2021-06-12 08:51:06 +0700

    first commit

---
+++ include/config.php
@@ -0,0 +1,14 @@
+<?php
+    session_start();
+
+    // connect to database
+    $host = 'localhost';
+    $user = 'root';
+    $pass = '';
+    $db_name = 'thehours';
+
+    $conn = new MySQLi($host, $user, $pass, $db_name);
+
+    if ($conn->connect_error) {
+        die('Database connection error: ' . $conn->connect_error);
+    }
---
+++ include/db-functions.php
@@ -0,0 +1,34 @@
+<?php
+require('config.php');
+
+/* one for all */
+function GetAll($table, $condition = []){
+    global $conn;
+    $sql = "SELECT * FROM products";
+    $result = mysqli_query($conn, $sql);
+
+    $products = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    $final = array();
+    foreach ($products as $product) {
+        array_push($final, $product);
+    }
+    return $final;
+}
+
+/* ARTICLES */
+function GetAllPosts(){
+    global $conn;
+    $sql = "SELECT * FROM products";
+    $result = mysqli_query($conn, $sql);
+
+    $products = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    $final = array();
+    foreach ($products as $product) {
+        array_push($final, $product);
+    }
+    return $final;
+}
+
+?>
