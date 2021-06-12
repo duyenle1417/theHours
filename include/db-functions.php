d8e852b93260eb1e965705f00981451009c10975
duyenle1417 <18520680@gm.uit.edu.vn>
2021-06-12 09:04:44 +0700

    first commit

--- include/db-functions.php
+++ include/db-functions.php
@@ -2,18 +2,30 @@
 require('config.php');
 
 /* one for all */
-function GetAll($table, $condition = [])
+function GetAll($table, $condition = [])//condition [key => value]
 {
-    // global $conn;
-    // // $sql = "SELECT * FROM products";
-    // // $result = mysqli_query($conn, $sql);
-
-    // // $products = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
-    // // $final = array();
-    // // foreach ($products as $product) {
-    // //     array_push($final, $product);
-    // // }
+    global $conn;
+    $sql = "SELECT * FROM $table";
+    if (empty($conditions)) {
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        $records = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $records;
+    } else {
+        $i = 0;
+        foreach ($conditions as $key => $value) {
+            if ($i === 0) {
+                $sql = $sql . " WHERE $key=?";
+            } else {
+                $sql = $sql . " AND $key=?";
+            }
+            $i++;
+        }
+        
+        $stmt = executeQuery($sql, $conditions);
+        $records = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $records;
+    }
     return $final;
 }
 
