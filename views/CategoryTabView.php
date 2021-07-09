7cc6495b2be502a8362ab5a4352131fab23fa5d9
duyenle1417 <18520680@gm.uit.edu.vn>
2021-07-09 15:01:50 +0700

    tab click

--- views/CategoryTabView.php
+++ views/CategoryTabView.php
@@ -30,3 +30,26 @@ $tab_recent = $post_model->GetPostsByTopicTab($topic['id'], 10, 'id');
         <?php } ?>
     </div>
 </div>
+
+<script>
+    function openTab(evt, tabName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("nav-tabs");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+</script>
